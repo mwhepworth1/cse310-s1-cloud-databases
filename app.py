3ae51c3213632ee8c4053093ce580d5e8a2ed7ca
vuulgen <97462952+vuulgen@users.noreply.github.com>
2025-01-23 13:06:48 -0700

    Update for front end

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 # Import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from api import api_bp
 
 # Create an instance of the Flask class
 # The first argument is the name of the application's module or package
 app = Flask(__name__)
-
 app.register_blueprint(api_bp)
 
 #######################################
 #             FRONT END               #
 #######################################
 
+# In-memory storage for to-do items
+todo_items = []
+
 # Define a route for the root URL ("/")
 @app.route('/')
 def home():
-    # List of items to display
-    items = ["Item 1", "Item 2", "Item 3", "Item 4"]
-    # Render the template and pass the list of items
-    return render_template('index.html', items=items)
+    return render_template('index.html', items=todo_items)
+
+# Define a route to add new to-do items
+@app.route('/add', methods=['POST'])
+def add_item():
+    item = request.form.get('item')
+    if item:
+        todo_items.append(item)
+    return redirect(url_for('home'))
 
 # Check if the executed file is the main program and run the app
 if __name__ == '__main__':
-    # Run the Flask application
-    # debug=True enables debug mode, which provides detailed error pages and auto-reloads the server on code changes
     app.run(debug=True)
